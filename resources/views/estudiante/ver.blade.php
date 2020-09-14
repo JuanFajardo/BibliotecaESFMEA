ce3889466e7d17ca6a856c15350565d2a6fc8dc8
Juan Fajardo <bett0@potosi.bo>
2020-09-14 17:13:11 -0400

    @view verPDF

--- resources/views/estudiante/ver.blade.php
+++ resources/views/estudiante/ver.blade.php
@@ -13,9 +13,9 @@ Libro - {{$libro->titulo}}
 <!-- Libros -->
 @if($ver != "NO")
 <div style="position:relative;width:100%;">
-    <div style="width:100%; background:#000;height:105px;">
+    <div style="width:100%; background:#000;height:215px;">
     </div>
-    <iframe src="{{$libro->archivo}}" style="width:100%;height:760px;"></iframe>
+    <iframe src="{{$libro->archivo}}" style="width:100%;height:960px;"></iframe>
 </div>
 @else
 <label class="btn btn-danger"> Su numero de intentos de visualización llego al limite</label>
